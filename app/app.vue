23d7987290cc2fa5c6325653cc7d151af242c966
Alex Ivanov <oleksii120@gmail.com>
2025-08-29 14:20:49 +0300

    Add test pages

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <a href="https://neocities.org/" style="display: inline-block; margin-top: 1rem;">
+    <NuxtImg src="/neocities.png" alt="Neocities Logo" height="3rem" />
+  </a>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+        <h1>Rojikaru crib</h1>
+        <p>
+            Hello, world!
+            One day, I will be a website of that lazy dude who never updates it.
+        </p>
+    </main>
+</template>
