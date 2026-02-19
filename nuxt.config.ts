67852bd24dff43e98a476754c1a110876d903232
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-20 00:45:36 +0200

    perf(config): route rules for caching assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: process.env.NODE_ENV === 'development' },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: process.env.NODE_ENV === "development" },
 
   nitro: {
-    preset: 'static'
+    preset: "static",
   },
 
   modules: [
-    '@tailwindcss/vite',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/content',
-    '@nuxt/fonts',
+    "@tailwindcss/vite",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@nuxt/fonts",
   ],
 
-  css: ['~~/styles/global.css'],
-})
+  css: ["~~/styles/global.css"],
+
+  routeRules: {
+    "/_nuxt/**": {
+      headers: {
+        "Cache-Control": "public, max-age=31536000, immutable",
+      },
+    },
+    "/_fonts/**": {
+      headers: {
+        "Cache-Control": "public, max-age=31536000, immutable",
+      },
+    },
+  },
+});
