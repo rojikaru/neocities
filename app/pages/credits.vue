04fe609f9b0c3f89ea58b2b42bb0a45724ec1878
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-22 01:25:24 +0200

    feat: credits page

---
+++ app/pages/credits.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { profileLink, email, sourceCode } from "~/constants/contacts";
+
+useHead({
+  title: "Credits",
+});
+</script>
+
+<template>
+  <article class="space-y-4">
+    <section>
+      <h1>Credits</h1>
+      <p>This site was made by me, <a :href="profileLink">rojikaru</a>.</p>
+      <p>
+        The source code is available on
+        <a :href="sourceCode">GitHub</a>.
+      </p>
+      <p>If you want to contact me, you can email me at {{ email }}.</p>
+    </section>
+    <section>
+      <h2 class="text-xl font-bold leading-loose">Acknowledgements</h2>
+      <ul>
+        <li>
+          Thanks to the Neocities community for their support and feedback.
+        </li>
+      </ul>
+    </section>
+    <section>
+        <h2>Resources</h2>
+        <ul>
+          <li>
+            <a href="https://nuxt.com/">Nuxt</a> - The Vue framework used to build this site.
+          </li>
+          <li>
+            <a href="https://streamlinehq.com/" title="Pixel icons">Streamline Icons</a>
+            - The source of some of the icons here
+          </li>
+        </ul>
+    </section>
+  </article>
+</template>
