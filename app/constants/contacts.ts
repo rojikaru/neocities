17b4957d14c7bfb218b79b2febcc9fdedee76174
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-21 22:19:05 +0200

    refactor: global basic styles

---
+++ app/constants/contacts.ts
@@ -0,0 +1,3 @@
+export const profileLink = "https://neocities.org/site/rojikaru";
+export const sourceCode = "https://github.com/rojikaru/neocities";
+export const email = "omegatuple /at/ gmail.com";
