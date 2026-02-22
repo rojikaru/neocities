1c95f729664fa0e30589e010d881f461b13c75f2
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-22 03:11:38 +0200

    fix(music): point to github bc neocities doesn't support music

--- app/constants/music.ts
+++ app/constants/music.ts
@@ -1,6 +1,13 @@
-export const musicPaths = [
+const originalMusicPaths = [
   "/music/omori-not-so-empty-house.ogg",
   "/music/sans.mp3",
   "/music/nicosnextbotsost-saferoom.mp3",
   "/music/undertale-home.mp3",
 ] as const;
+
+const hostingPrefix =
+  "https://raw.githubusercontent.com/rojikaru/neocities/refs/heads/main/public";
+
+export const musicPaths = originalMusicPaths.map(
+  (path) => `${hostingPrefix}${path}` as const,
+);
